f560026824514181f84b142e7408ddabd1fb5f7a
Nathan Moreira <nathangc7@gmail.com>
2024-12-11 15:40:50 -0300

    first commit

---
+++ src/main/java/com/moreira/techpoint/TechpointApplication.java
@@ -0,0 +1,13 @@
+package com.moreira.techpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moreira/techpoint/TechpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreira.techpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
