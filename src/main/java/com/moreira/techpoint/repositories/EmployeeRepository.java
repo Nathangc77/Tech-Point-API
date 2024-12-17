5e7e9f053379f5277f7cbce3008febe512e6a60b
Nathan Moreira <nathangc7@gmail.com>
2024-12-17 15:20:02 -0300

    UserDetailsService not tested

--- src/main/java/com/moreira/techpoint/repositories/EmployeeRepository.java
+++ src/main/java/com/moreira/techpoint/repositories/EmployeeRepository.java
@@ -1,7 +1,23 @@
 package com.moreira.techpoint.repositories;
 
 import com.moreira.techpoint.entities.Employee;
+import com.moreira.techpoint.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT tb_employee.employeeCode AS username, tb_employee.password, tb_role.id AS roleId, tb_role.authority
+            FROM tb_employee
+            INNER JOIN tb_employee_role ON tb_employee.id = tb_employee_role.employee_id
+            INNER JOIN tb_role ON tb_employee_role.role_id = tb_role.id
+            WHERE tb_employee.employee_code = :employeeCode
+            """)
+    List<UserDetailsProjection> searchEmployeeAndRolesByEmployeeCode(String employeeCode);
 }
--- src/main/java/com/moreira/techpoint/services/EmployeeService.java
+++ src/main/java/com/moreira/techpoint/services/EmployeeService.java
@@ -3,15 +3,20 @@ package com.moreira.techpoint.services;
 import com.moreira.techpoint.dtos.EmployeeDTO;
 import com.moreira.techpoint.dtos.EmployeeMinDTO;
 import com.moreira.techpoint.entities.Employee;
+import com.moreira.techpoint.entities.Role;
+import com.moreira.techpoint.projections.UserDetailsProjection;
 import com.moreira.techpoint.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-public class EmployeeService {
+public class EmployeeService implements UserDetailsService {
 
     @Autowired
     private EmployeeRepository repository;
@@ -33,5 +38,23 @@ public class EmployeeService {
         entity = repository.save(entity);
 
         return new EmployeeDTO(entity);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchEmployeeAndRolesByEmployeeCode(username);
+
+        if (result.isEmpty())
+            throw new UsernameNotFoundException("Usuário não encontrado");
+
+        Employee employee = new Employee();
+        employee.setEmployeeCode(username);
+        employee.setPassword(result.getFirst().getPassword());
+
+        for (UserDetailsProjection proj: result) {
+            employee.addRole(new Role(proj.getRoleId(), proj.getAuthority()));
+        }
+
+        return employee;
     }
 }
